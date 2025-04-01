42294dff3c3e16f3f2d565d3abbf6e0724974117
Ferguson Earnshaw <earnshae@users.noreply.github.com>
2025-03-31 21:54:53 -0600

    Added routing to sample app.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
@@ -7,5 +11,19 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+
+  housingService = inject(HousingService);
+
+  housingLocation: HousingLocation | undefined;
+
+  
+
+  constructor() {
+    const housingLocationID = Number(this.route.snapshot.params['id']);
+
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationID);
+  
+  }
 
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [ CommonModule, RouterModule ],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
